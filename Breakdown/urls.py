f94250baee04f1d30b18ef30de16f6ed85a3afb1
rapavin <rapavin@gmail.com>
2021-07-26 22:54:04 -0500

    Update urls.py

--- Breakdown/urls.py
+++ Breakdown/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.Home_Page, name='Home_Page'),
-    path('Parser_Page',views.Parser_Page, name='Parser_Page'),
+    path('Cisco_Parser_Page',views.Cisco_Parser_Page, name='Cisco_Parser_Page'),
+    path('Aruba_Parser_Page',views.Aruba_Parser_Page, name='Aruba_Parser_Page'),
     path('Network_Page',views.Network_Page, name='Network_Page'),
     path('upload_read',views.upload_read, name='upload_read'),
 ]
